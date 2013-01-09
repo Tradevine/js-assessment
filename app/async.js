5848bbff0f01756546d62d84282c84afb5e89fe0
dev <dev@nodejs-vm.(none)>
2013-01-10 12:08:40 +1300

    Clarification for jQuery usage

--- app/async.js
+++ app/async.js
@@ -1,6 +1,9 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define([ 'jquery' ], function($) {
+  /**
+   * Use jQuery helpers where it is applicable.
+   */
   return {
     async : function(value) {
 
