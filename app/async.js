44ad5d826ba8265cf67973d424eea823557852fc
Nigel Nguyen <nigel@nng.me>
2013-09-26 14:30:00 +1200

    update with more tests

--- app/async.js
+++ app/async.js
@@ -1,9 +1,6 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define([ 'jquery' ], function($) {
-  /**
-   * Use jQuery helpers where it is applicable.
-   */
   return {
     async : function(value) {
 
